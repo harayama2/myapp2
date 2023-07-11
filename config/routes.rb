73cb49cfda9425937edcb138a68f6ff6ad7f777c
harayama2 <44060633+harayama2@users.noreply.github.com>
2023-07-11 17:33:06 +0900

    Add different routing for login users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root "dashboard#show", as: :user_root
+  end
+
   get "about" => "static#about"
   get "policy" => "static#policy"
   get "terms" => "static#terms"
 
-  # Defines the root path route ("/")
   root "home#index"
 end
